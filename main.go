9e0076282f9a50dfb69e8e4bf775394ec645642b
Nigel Deakin <nigel.deakin@oracle.com>
2018-09-26 12:08:26 +0100

    Initial version

---
+++ main.go
@@ -0,0 +1,32 @@
+//   Copyright © 2018, Oracle and/or its affiliates.  All rights reserved.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func defaultHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	res.Write([]byte("Hello World!"))
+}
+
+func main() {
+	http.HandleFunc("/", defaultHandler)
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		log.Fatal("Unable to listen on port 5000 : ", err)
+	}
+}
